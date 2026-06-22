c6fc9e6bc5b7de7b7eae9a64b9b92bca123ab726
Sayan the researcher <sm6881164@gmail.com>
2026-06-22 14:02:17 +0530

    Create __init__.py

---
+++ modules/auth/__init__.py
@@ -0,0 +1,7 @@
+from .brute_force import run as run_brute
+from .hash_check import guess_hash
+from .credential_test import run as run_auth_check
+
+def run_auth_check(targets, config):
+    return run_auth_check(targets, config)
+  
